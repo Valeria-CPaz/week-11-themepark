5519c4b9b39d0acad18fa9188088a4de12c25046
Valeria Paz <21214@student.dorset-college.ie>
2020-12-01 12:53:05 +0000

    first commit

---
+++ src/com/val/Main.java
@@ -0,0 +1,22 @@
+package com.val;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        90 people in line
+//                every 5 min 40 peple join the ride and 63 join the line
+//                how long to line be 600 people
+
+        int line = 90;
+        int min = 0;
+        int endTime = 0;
+
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++){
+                int
+            }
+
+        }
+    }
+}
